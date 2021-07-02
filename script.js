c95adfc9cc3f8daffebe346fdaee98010e566561
Surabhi Srivastava <72160349+Surabhi2000@users.noreply.github.com>
2021-07-03 02:06:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let body= document.querySelector(".container");
 let toggle= document.getElementById("toggle");
+var x = document.getElementById("myDIV");
 
 toggle.addEventListener('click',e =>{
     if(toggle.checked){
@@ -7,4 +8,10 @@ toggle.addEventListener('click',e =>{
     }else{
         body.classList.remove('dark')
     }
+
+    if (x.innerHTML === "Coding at Night") {
+        x.innerHTML = "Coding at Day";
+     } else {
+        x.innerHTML = "Coding at Night";
+  }
 })
